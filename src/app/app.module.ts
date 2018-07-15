00a96395687aa8eb2951e61b5fc9b3610176799c
Camile Campos <camilecampos@outlook.com>
2018-07-15 13:09:16 -0300

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { OperatorComponent } from './operator/operator.component';
 import { OperatorService } from './operator/operator.service';
 
+const appRoutes: Routes = [
+  { path: 'operator', component: OperatorComponent},
+];
+
 @NgModule({
   imports: [
     BrowserModule,
     HttpModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   declarations: [
     AppComponent,
